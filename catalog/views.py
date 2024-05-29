b87c02dbf42ee0d1de695bfc393223226bc4886d
Mihail Sevostyanov <sevamihael141095@gmail.com>
2024-05-29 19:37:41 +0300

    Дополнительное задание

--- catalog/views.py
+++ catalog/views.py
@@ -4,4 +4,10 @@ def home(request):
     return render(request, template_name='home.html')
 
 def contacts(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+        print(f'{name}, {phone}, {message}')
     return render(request, template_name='contacts.html')
+
