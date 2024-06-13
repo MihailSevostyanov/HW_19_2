aaa1b7486d20f23da69d3a4aafdc77c216042075
Mihail Sevostyanov <sevamihael141095@gmail.com>
2024-06-13 13:53:09 +0300

    ДЗ 20.2 Задание 1 и 2 Создание контроллера для отображения 1 продукта Настройка отображения всех продуктов на странице

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from catalog.apps import CatalogConfig
-from catalog.views import product_list, contacts
+from catalog.views import product_list, contacts, product_detail
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path("", product_list),
-    path("contacts/", contacts),
+    path("", product_list, name='product_list'),
+    path("contacts/", contacts, name='contacts'),
+    path("products/int:<pk>", product_detail, name='product_detail')
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -18,3 +18,9 @@ def contacts(request):
     return render(request, template_name="contacts.html")
 
 
+def product_detail(request, pk):
+    product = Product.objects.get(pk=pk)
+    context = {'product': product}
+    return render(request, 'product_detail.html', context)
+
+
