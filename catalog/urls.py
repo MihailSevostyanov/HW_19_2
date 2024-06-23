be8242a225866cb2ae89590e921f8f3d5877a974
Mihail Sevostyanov <sevamihael141095@gmail.com>
2024-06-23 20:33:39 +0300

    HW 21.1 fix_2

--- catalog/urls.py
+++ catalog/urls.py
@@ -7,5 +7,8 @@ app_name = CatalogConfig.name
 urlpatterns = [
     path("products/", ProductListView.as_view(), name='product_list'),
     path("contacts/", ContactsView.as_view(), name='contacts'),
-    path("product/<int:pk>", ProductDetailView.as_view(), name='product_detail')
+    path("product/<int:pk>", ProductDetailView.as_view(), name='product_detail'),
+    # path("products/create/", ProductCreateView.as_view(), name='product_create'),
+    # path("products/<int:pk>/update/", ProductUpdateView.as_view(), name='product_update'),
+    # path("products/<int:pk>/delete/", ProductDeleteView.as_view(), name='product_delete')
 ]
